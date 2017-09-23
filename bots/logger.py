f52cb5c11cbb8b731e9553cfe79e950f88ed8484
NAKAJIMA Susumu <ueda2008+git@gmail.com>
2017-09-24 00:10:03 +0900

    add FileLogger filter

---
+++ bots/logger.py
@@ -0,0 +1,37 @@
+
+from multiprocessing import Process, Queue
+from queue import Empty, Full
+import collections
+import traceback
+import datetime
+
+from bots.common import Status, FilterBase
+
+class FileLogger(FilterBase):
+    def __init__(self, confs):
+        super().__init__(confs['filtername'])
+        self.logdir = confs.get('dir', '.')
+        self.logfile = confs.get('file', '{}.log'.format(self.filtername))
+        self.template = confs.get('template', '{status.sender}: {time} {status.data[level]} {status.data[message]}')
+
+    def run(self):
+        while True:
+            try:
+                self.run_()
+            except Exception as e:
+                print(traceback.format_exc())
+
+    def onmessage(self, message):
+        self.write(message)
+        
+        message.sender = self.filtername
+        self.Q.put(message)
+
+    def write(self, message):
+        now = datetime.datetime.now()
+        line = self.template.format(status=message, time=now)
+        with open(self.getfile(), 'a') as fp:
+            print(line, file=fp)
+
+    def getfile(self):
+        return self.logdir + '/' + self.logfile
---
+++ test/bots/Logger.py
@@ -0,0 +1,57 @@
+
+import unittest
+import tempfile
+import os
+import os.path
+import queue
+
+from bots.logger import FileLogger
+from bots.common import Status
+
+class FileLoggerTest(unittest.TestCase):
+    def setUp(self):
+        self.logdir = tempfile.mkdtemp()
+        self.logfile = 'test.log'
+
+    def tearDown(self):
+        for root, dirs, files in os.walk(self.logdir, topdown=False):
+            for name in files:
+                os.remove(os.path.join(root, name))
+            for name in dirs:
+                os.rmdir(os.path.join(root, name))
+        
+    def test_output(self):
+        Q = queue.Queue()
+
+        f = FileLogger({
+            'filtername': 'test',
+            'dir': self.logdir,
+            'file': self.logfile
+        })
+        f.Q = Q
+
+        status = Status('sender', 'sys.log', {
+            'level': 'INFO',
+            'message': 'logmessage1'
+        })
+        f.onmessage(status)
+
+        ret = Q.get(0)
+        self.assertTrue(Q.empty())
+        self.assertEqual(ret.sender, 'test')
+        self.assertEqual(ret.type, 'sys.log')
+        self.assertEqual(ret.data, {
+            'level': 'INFO',
+            'message': 'logmessage1'
+        })
+
+        path = self.logdir + '/' + self.logfile
+        self.assertTrue(os.path.exists(path))
+        with open(path, 'r') as fp:
+            contents = fp.read()
+            self.assertTrue('INFO' in contents)
+            self.assertTrue('logmessage1' in contents)
+            self.assertTrue('sender' in contents)
+
+if __name__ == '__main__':
+    unittest.main()
