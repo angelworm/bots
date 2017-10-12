27f5cc37fd5c50d231b05fbcf5291b95c3c1286a
NAKAJIMA Susumu <ueda2008+git@gmail.com>
2017-10-12 20:35:53 +0900

    extract discord message embedding

--- bots/discord.py
+++ bots/discord.py
@@ -2,6 +2,7 @@
 import requests
 import discord
 import json
+import asyncio
 
 from .common import Status, FilterBase
 
@@ -23,20 +24,28 @@ class DiscordClient(FilterBase):
         finally:
             if self.client is not None:
                 self.client.close()
+            loop = asyncio.get_event_loop()
+            loop.close()
+            policy = asyncio.get_event_loop_policy()
+            policy.set_event_loop(policy.new_event_loop())
 
     def on_message(self, message):
         dispname, name = self.getauthorname(message.author)
+
+        servername = ''
+        if message.server is not None:
+            servername = message.server.name
         
         data = {
-            'server': message.server,
-            'channel': message.channel,
+            'server': servername,
+            'channel': message.channel.name,
             'account': {
                 'disp': dispname,
                 'name': name,
                 'avatar': self.getavatar(message.author)
             },
             'timestamp': message.timestamp,
-            'content': message.clean_content
+            'content': self.getcontent(message)
         }
         
         self.Q.put(Status(self.filtername, 'discord.update', data))
@@ -52,4 +61,33 @@ class DiscordClient(FilterBase):
             author.id,
             author.avatar
         )
-        
+
+    def getcontent(self, message):
+        if message.type is discord.MessageType.default:
+            ret = message.clean_content
+            if ret is None:
+                ret = message.system_content
+            if ret is None:
+                ret = ''
+            
+            for e in message.embeds:
+                ret += " " + self.getembettext(e)
+            for a in message.attachments:
+                ret += " " + a.get('url', str(a))
+            return ret
+        else:
+            return message.system_content
+
+    def getembettext(self, embed):
+        ret = None
+        if embed['type'] == 'rich':
+            ret = embed.get('description', '')
+        elif embed['type'] == 'photo':
+            ret = embed.get('url', '')
+        elif embed['type'] == 'video':
+            ret = embed.get('html', '')
+        elif embed['type'] == 'link':
+            ret = str(embed)
+        if ret is None:
+            ret = 'None'
+        return ret
